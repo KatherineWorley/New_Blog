bbe3bdda2293fcc3a4a8364d98177644b4c62139
Katherine Worley <kdesign216@yahoo.com>
2018-01-15 08:27:46 -0500

    updated posts controller and index/new views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,18 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		
+		@post = Post.new(post_params)
+		@post.save
+
+		redirect_to @post 
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	private
+	def post_params
+		params.require(:post).permit(:title, :body)
 	end
 end
